41009a09a82753c84db5fb6f2861037dbe02e84e
Renan <renmms42@gmail.com>
2021-09-21 16:13:37 -0300

    App, data, index, review

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Eduarda Pereira",
+    job: "Web Developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "Uma desenvolvedora que adora violão e desenhos",
+  },
+  {
+    id: 2,
+    name: "Matheus da Silva",
+    job: "Mecânico",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Apaixonado por carros, futebol e churrasco",
+  },
+  {
+    id: 3,
+    name: "Judith",
+    job: "Telemarketing",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Especialista em resolver o problema dos clientes",
+  },
+  {
+    id: 4,
+    name: "Lucas André",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Empresário de sucesso",
+  },
+];
+
+export default reviews;
